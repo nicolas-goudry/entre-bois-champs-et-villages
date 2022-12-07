04a79ee46ab9c43aa8c0f2b839c2d7aadad9112b
Nicolas Goudry <nicolas@kubolabs.io>
2022-12-07 23:22:26 +0100

    chore(gatsby): update site URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,26 @@
 module.exports = {
   siteMetadata: {
     title: `Entre bois, champs et villages`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.entreboischampsetvillages.fr`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
     },
-    __key: "images"
-  }]
-};
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}

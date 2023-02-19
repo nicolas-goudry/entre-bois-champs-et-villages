1f2480f81e0f78a53a7ec44002247109f45a936c
Nicolas Goudry <goudry.nicolas@gmail.com>
2023-02-19 15:59:07 +0100

    feat(layout): finalize layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,103 @@
+import { Box, Divider, Flex, Heading, Link, ListItem, OrderedList, Text, UnorderedList } from '@chakra-ui/react'
+import { MDXProvider } from '@mdx-js/react'
+import { Link as GatsbyLink } from 'gatsby'
 import React from 'react'
 
 import Header from '../components/header'
+import FacebookFeed from '../components/facebook-feed'
 import Footer from '../components/footer'
-import { Flex } from '@chakra-ui/layout'
 
-export default function LayoutIndex({ children }) {
+const mdxComponents = {
+  a({ href, children }) {
+    return (
+      <Link as={GatsbyLink} href={href} to={href} target={href.startsWith('#') ? '_self' : '_blank'} color='green.500'>
+        {children}
+      </Link>
+    )
+  },
+  h1({ children }) {
+    return (
+      <Heading as='h1' size='3xl'>
+        {children}
+      </Heading>
+    )
+  },
+  h2({ children }) {
+    return (
+      <Heading as='h2' size='2xl'>
+        {children}
+      </Heading>
+    )
+  },
+  h3({ children }) {
+    return (
+      <Heading as='h3' size='xl'>
+        {children}
+      </Heading>
+    )
+  },
+  h4({ children }) {
+    return (
+      <Heading as='h4' size='lg'>
+        {children}
+      </Heading>
+    )
+  },
+  h5({ children }) {
+    return (
+      <Heading as='h5' size='md'>
+        {children}
+      </Heading>
+    )
+  },
+  h6({ children }) {
+    return (
+      <Heading as='h6' size='sm'>
+        {children}
+      </Heading>
+    )
+  },
+  strong({ children }) {
+    return <Text as='strong'>{children}</Text>
+  },
+  em({ children }) {
+    return <Text as='em'>{children}</Text>
+  },
+  hr() {
+    return <Divider />
+  },
+  ol({ children }) {
+    return (
+      <Box as='p' mb={4}>
+        <OrderedList>{children}</OrderedList>
+      </Box>
+    )
+  },
+  ul({ children }) {
+    return (
+      <Box as='p' mb={4}>
+        <UnorderedList>{children}</UnorderedList>
+      </Box>
+    )
+  },
+  li({ children }) {
+    return <ListItem>{children}</ListItem>
+  },
+  p({ children }) {
+    return (
+      <Box as='p' mb={4}>
+        {children}
+      </Box>
+    )
+  },
+  FacebookFeed,
+}
+
+export default function LayoutIndex({ children, location }) {
   return (
     <Flex direction='column' minH='100vh'>
-      <Header />
-      {children}
+      <Header location={location} />
+      <MDXProvider components={mdxComponents}>{children}</MDXProvider>
       <Footer />
     </Flex>
   )
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,92 +1,10 @@
-import { Box, Divider, Heading, Link, ListItem, OrderedList, Text, UnorderedList } from '@chakra-ui/react'
-import { Link as GatsbyLink, graphql } from 'gatsby'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import React from 'react'
-import rehypeReact from 'rehype-react'
 
-const renderAst = new rehypeReact({
-  createElement: React.createElement,
-  components: {
-    a({ href, children }) {
-      return (
-        <Link as={GatsbyLink} href={href} to={href} target={href.startsWith('#') ? '_self' : '_blank'}>
-          {children}
-        </Link>
-      )
-    },
-    h1({ children }) {
-      return (
-        <Heading as='h1' size='3xl'>
-          {children}
-        </Heading>
-      )
-    },
-    h2({ children }) {
-      return (
-        <Heading as='h2' size='2xl'>
-          {children}
-        </Heading>
-      )
-    },
-    h3({ children }) {
-      return (
-        <Heading as='h3' size='xl'>
-          {children}
-        </Heading>
-      )
-    },
-    h4({ children }) {
-      return (
-        <Heading as='h4' size='lg'>
-          {children}
-        </Heading>
-      )
-    },
-    h5({ children }) {
-      return (
-        <Heading as='h5' size='md'>
-          {children}
-        </Heading>
-      )
-    },
-    h6({ children }) {
-      return (
-        <Heading as='h6' size='sm'>
-          {children}
-        </Heading>
-      )
-    },
-    strong({ children }) {
-      return <Text as='strong'>{children}</Text>
-    },
-    em({ children }) {
-      return <Text as='em'>{children}</Text>
-    },
-    hr() {
-      return <Divider />
-    },
-    ol({ children }) {
-      return (
-        <p>
-          <OrderedList>{children}</OrderedList>
-        </p>
-      )
-    },
-    ul({ children }) {
-      return (
-        <p>
-          <UnorderedList>{children}</UnorderedList>
-        </p>
-      )
-    },
-    li({ children }) {
-      return <ListItem>{children}</ListItem>
-    },
-    p({ children }) {
-      return <p>{children}</p>
-    },
-  },
-}).Compiler
+import FacebookFeed from '../components/facebook-feed'
+import Markdown from '../components/markdown'
 
 export const pageQuery = graphql`
   query Page($path: String!) {
@@ -99,67 +17,76 @@ export const pageQuery = graphql`
           }
         }
       }
-      childMarkdownRemark {
-        htmlAst
-      }
+      markdown
     }
   }
 `
 
-export default function PageTemplate({
-  data: {
-    page: {
-      name,
-      cover,
-      childMarkdownRemark: { htmlAst },
-    },
-  },
-}) {
+export default function PageTemplate({ data }) {
+  const cover = data?.page?.cover
   const coverProps = {
     style: {
-      width: '100%',
       height: '24rem',
       maxHeight: '24rem',
+      width: '100%',
     },
     objectPosition: 'center 60%',
   }
 
-  if (cover.title) {
+  if (cover?.title) {
     coverProps.title = cover.title
   }
 
-  if (cover.alt) {
+  if (cover?.alt) {
     coverProps.alt = cover.alt
   }
 
   return (
     <>
-      <Box w='100%' h={96} maxH={96} pos='relative'>
-        {/*
-        To add a cover:
-        Add an image in your Google Doc first page header
-        https://support.google.com/docs/answer/86629
-      */}
-        {cover && <GatsbyImage image={getImage(cover.image)} {...coverProps} />}
-        <Box px={4} m='auto' maxW='6xl' borderRadius={1}>
-          <Heading
-            as='h1'
-            px={8}
-            py={4}
-            size='3xl'
-            zIndex={1}
-            bg='blackAlpha.600'
-            color='white'
-            pos='absolute'
-            bottom={8}
-          >
-            {name}
-          </Heading>
+      {cover && (
+        <Box h={96} maxH={96} pos='relative' w='100%'>
+          {cover?.image && <GatsbyImage image={getImage(cover.image)} {...coverProps} />}
+          {data?.page?.name && (
+            <Box borderRadius={1} m='auto' maxW='6xl' px={4}>
+              <Heading
+                as='h1'
+                bg='blackAlpha.600'
+                bottom={8}
+                color='white'
+                pos='absolute'
+                px={8}
+                py={4}
+                size='3xl'
+                zIndex={1}
+              >
+                {data.page.name}
+              </Heading>
+            </Box>
+          )}
         </Box>
-      </Box>
+      )}
       <Box as='main' flex={1}>
-        <Box px={4} py={2} mx='auto' my={8} w='100%' maxW='6xl'>
-          {renderAst(htmlAst)}
+        <Box maxW='6xl' mx='auto' my={8} w='100%'>
+          <Flex direction={{ base: 'column', lg: 'row' }}>
+            <Box px={4}>
+              <Markdown>{data?.page?.markdown}</Markdown>
+            </Box>
+            {data?.page?.name === 'Accueil' && (
+              <Box bg='orange.100' borderRadius={4} mx={4} p={4}>
+                <Text align='center' as='strong' display='inline-block'>
+                  Retrouvez toutes les actualités de l’association sur notre page Facebook !
+                </Text>
+                <FacebookFeed
+                  smallHeader
+                  appId='1891293047900564'
+                  cover={false}
+                  cta={false}
+                  facepile={false}
+                  href='https://www.facebook.com/Entreboischampsetvillages'
+                />
+              </Box>
+            )}
+          </Flex>
         </Box>
       </Box>
     </>

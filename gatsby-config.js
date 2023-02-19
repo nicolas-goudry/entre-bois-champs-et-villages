ebb3274a9e4ed56a0ecb63e6b3a4ae9bdfbff56a
Nicolas Goudry <goudry.nicolas@gmail.com>
2023-02-19 15:59:06 +0100

    chore(gatsby): configure chakra-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,25 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+        /**
+         * @property {boolean} [isBaseProvider=false]
+         * if true, will render `<ChakraBaseProvider>`
+         */
+        isBaseProvider: false,
+      },
+    },
   ],
 }

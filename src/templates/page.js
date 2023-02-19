df5b3129a545417c404cdf6b0014094407f758c0
Nicolas Goudry <goudry.nicolas@gmail.com>
2023-02-19 15:59:06 +0100

    feat(tpl/page): improve presentation (styles)

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,4 +1,4 @@
-import { Button, Divider, Heading, Link, ListItem, OrderedList, Text, UnorderedList } from '@chakra-ui/react'
+import { Box, Divider, Heading, Link, ListItem, OrderedList, Text, UnorderedList } from '@chakra-ui/react'
 import { Link as GatsbyLink, graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import React from 'react'
@@ -115,20 +115,53 @@ export default function PageTemplate({
     },
   },
 }) {
+  const coverProps = {
+    style: {
+      width: '100%',
+      height: '24rem',
+      maxHeight: '24rem',
+    },
+    objectPosition: 'center 60%',
+  }
+
+  if (cover.title) {
+    coverProps.title = cover.title
+  }
+
+  if (cover.alt) {
+    coverProps.alt = cover.alt
+  }
+
   return (
     <>
-      <GatsbyLink to='/'>
-        <Button>Home</Button>
-      </GatsbyLink>
-      <Heading as='h1' size='3xl'>{name}</Heading>
-      {/*
+      <Box w='100%' h={96} maxH={96} pos='relative'>
+        {/*
         To add a cover:
         Add an image in your Google Doc first page header
         https://support.google.com/docs/answer/86629
       */}
-      {cover && <GatsbyImage image={getImage(cover.image)} title={cover.title} alt={cover.alt} />}
-      {/* <div dangerouslySetInnerHTML={{ __html: html }} /> */}
-      <div>{renderAst(htmlAst)}</div>
+        {cover && <GatsbyImage image={getImage(cover.image)} {...coverProps} />}
+        <Box px={4} m='auto' maxW='6xl' borderRadius={1}>
+          <Heading
+            as='h1'
+            px={8}
+            py={4}
+            size='3xl'
+            zIndex={1}
+            bg='blackAlpha.600'
+            color='white'
+            pos='absolute'
+            bottom={8}
+          >
+            {name}
+          </Heading>
+        </Box>
+      </Box>
+      <Box as='main' flex={1}>
+        <Box px={4} py={2} mx='auto' my={8} w='100%' maxW='6xl'>
+          {renderAst(htmlAst)}
+        </Box>
+      </Box>
     </>
   )
 }

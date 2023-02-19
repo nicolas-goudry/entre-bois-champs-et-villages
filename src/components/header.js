14b90a1d40da379b530cb18889fb730083e7d612
Nicolas Goudry <goudry.nicolas@gmail.com>
2023-02-19 15:59:07 +0100

    feat(cmp/header): finalize responsive header

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,52 @@
 import { Box, Flex, Text } from '@chakra-ui/layout'
-import React from 'react'
+import { useBreakpoint } from '@chakra-ui/media-query'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'gatsby'
+import React, { useState } from 'react'
 
 import Logo from '../assets/logo.svg'
+import FaIcon from './fa-icon'
+import Menu from './menu'
 
-export default function Header() {
+export default function Header({ location }) {
+  const bp = useBreakpoint()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
-    <Box as='header' bg='orange.800' color='orange.50'>
-      <Flex align='stretch' justify='space-between' px={4} py={2} w='100%' h={16} maxW='6xl' maxH={16} m='auto'>
-        <Flex gap={4} color='white' align='center'>
-          <Logo style={{ height: '100%'}} />
-          <Text as='span'>Entre Bois, Champs et Villages</Text>
+    <>
+      <Box as='header' bg='green.600' color='green.50'>
+        <Flex align='stretch' h={24} justify='space-between' m='auto' maxH={24} maxW='6xl' px={4} w='100%'>
+          <Flex
+            align='center'
+            as={Link}
+            color='green.50'
+            py={4}
+            to='/'
+            transition='color ease-in-out 300ms, background-color ease-in-out 300ms'
+            _hover={{ backgroundColor: 'green.50', color: 'green.600' }}
+          >
+            <Box as={Logo} filter='drop-shadow(2px 4px 2px rgb(0 0 0 / 0.4))' h={12} mx={4} />
+            <Text as='strong' fontSize={{ base: 16, md: 20 }}>
+              Entre Bois, Champs et Villages
+            </Text>
+          </Flex>
+          {['base', 'sm'].includes(bp) && (
+            <Flex
+              align='center'
+              cursor='pointer'
+              justify='center'
+              px={8}
+              py={4}
+              transition='color ease-in-out 300ms, background-color ease-in-out 300ms'
+              _hover={{ backgroundColor: 'green.50', color: 'green.600' }}
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <FaIcon icon={faBars} size='2x' />
+            </Flex>
+          )}
         </Flex>
-        <Text>MENU</Text>
-      </Flex>
-    </Box>
+      </Box>
+      <Menu isOpen={isMenuOpen} location={location} open={setIsMenuOpen} />
+    </>
   )
 }
